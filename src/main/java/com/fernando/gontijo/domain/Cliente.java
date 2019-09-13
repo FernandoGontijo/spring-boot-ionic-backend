4e101836590375b925cf4b61a2242e798295394b
FErnando Gontijo <fernando.gontijos@hotmail.com>
2019-09-13 17:29:00 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/fernando/gontijo/domain/Cliente.java
+++ src/main/java/com/fernando/gontijo/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fernando.gontijo.domain.enums.TipoCliente;
 
 
@@ -33,6 +34,9 @@ public class Cliente implements Serializable {
 	private Integer tipo; // Enum 
 	
 	
+	
+	
+	@JsonManagedReference									// Serializa o endereço
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList();
 	
---
+++ src/main/java/com/fernando/gontijo/services/ClienteService.java
@@ -0,0 +1,29 @@
+package com.fernando.gontijo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fernando.gontijo.domain.Cliente;
+import com.fernando.gontijo.repositories.ClienteRepository;
+import com.fernando.gontijo.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class ClienteService {
+
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) { // Método para buscar um obj no banco pelo ID 
+		
+		Optional<Cliente> obj = repo.findById(id); //sempre usar esse trechos para buscar obj por ID no Spring 2
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); // lança uma excessão caso o id não exista
+		
+	}
+	
+}
