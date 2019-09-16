1eb50703c4e355f5901e1b5ec8f6cb0544fed076
Fernando Gontijo <fernando.gontijos@hotmail.com>
2019-09-15 22:48:53 -0300

    poucas alteracoes

---
+++ src/main/java/com/fernando/gontijo/domain/ItemPedido.java
@@ -0,0 +1,13 @@
+package com.fernando.gontijo.domain;
+
+public class ItemPedido {
+	
+	
+	private Double desconto;
+	private Integer quantidade;
+	private Double preco;
+	
+	
+	
+
+}
---
+++ src/main/java/com/fernando/gontijo/domain/ItemPedidoPK.java
@@ -0,0 +1,23 @@
+package com.fernando.gontijo.domain;
+
+import java.io.Serializable;
+
+public class ItemPedidoPK  implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Pedido pedido;
+	private Produto produto;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
