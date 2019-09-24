c51c9c9aac41bf4dddd2dcef50399beef38266ed
Fernando Gontijo <fernando.gontijos@hotmail.com>
2019-09-23 22:28:38 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/fernando/gontijo/services/CategoriaService.java
+++ src/main/java/com/fernando/gontijo/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.fernando.gontijo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,5 +55,10 @@ public class CategoriaService {
 		
 	}
 	
+	
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
 	
 }
