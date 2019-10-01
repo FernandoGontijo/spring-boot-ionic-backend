bc8e46f496dc8b3f34599cdb1358fe5d17c49367
Fernando Gontijo <fernando.gontijos@hotmail.com>
2019-09-30 22:56:46 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/fernando/gontijo/services/ClienteService.java
+++ src/main/java/com/fernando/gontijo/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.fernando.gontijo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fernando.gontijo.domain.Cliente;
+import com.fernando.gontijo.dto.ClienteDTO;
 import com.fernando.gontijo.repositories.ClienteRepository;
+import com.fernando.gontijo.services.exceptions.DataIntegrityException;
 import com.fernando.gontijo.services.exceptions.ObjectNotFoundException;
 
 
@@ -24,6 +31,57 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); // lança uma excessão caso o id não exista
 		
+	}
+	
+	
+
+	public Cliente update(Cliente obj) {		 // Método para ATUALIZAR novo elemento ao Banco de Dados 
+		Cliente newObj = find(obj.getId());							// Metodo "Find" verifica se o id existe
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {				// Método para DELETAR novo elemento ao Banco de Dados 
+		find(id);
+		try {
+			
+			repo.deleteById(id);
+			
+		} catch (DataIntegrityViolationException e) {
+			
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			
+		}
+		
+	}
+	
+	
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	
+	
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){					// Paginação
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
+	}
+	
+	
+	
+	public Cliente fromDTO(ClienteDTO objDto) {			
+		
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {  // Atualiza somente algums atributos do objeto Cliente
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 	
 }
